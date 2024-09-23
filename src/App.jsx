4ce86baf9c6b924af55e7622988790cc7dd4ab61
Laurynas <klumbyslaurynas@gmail.com>
2024-09-23 22:10:10 +0300

    category jsx ir scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // import './App.css'
 
+
+import Category from "./components/category/Category"
 import Search from "./components/search/search"
 import TopBar from "./components/topBar/TopBar"
 
@@ -10,6 +12,13 @@ function App() {
     
       <TopBar />
       <Search />
+      <Category />
+      {/* <Category name="Cleaning" icon={<GiVacuumCleaner />} />
+      <Category name="Repair" icon={<GiAutoRepair />} />
+      <Category name="Painting" icon={<GiLargePaintBrush />} />
+      <Category name="Shifting" icon={<CiDeliveryTruck />} />
+      <Category name="Plumbing" icon={<MdOutlinePlumbing />} />
+      <Category name="Electric" icon={<MdElectricBolt />} /> */}
 
     </>
   )
---
+++ src/components/category/Category.jsx
@@ -0,0 +1,36 @@
+import style from './Category.module.scss';
+
+import { GiVacuumCleaner } from "react-icons/gi";
+import { GiAutoRepair } from "react-icons/gi";
+import { GiLargePaintBrush } from "react-icons/gi";
+import { CiDeliveryTruck } from "react-icons/ci";
+import { MdOutlinePlumbing } from "react-icons/md";
+import { MdElectricBolt } from "react-icons/md";
+
+function Category() {
+
+    const categories = [
+        { name: "Cleaning", icon: <GiVacuumCleaner />, color: 'purple'},
+        { name:"Repair", icon:<GiAutoRepair />, color: 'orange'},
+        { name:"Painting", icon: < GiLargePaintBrush />, color: 'lightseagreen'},
+        { name:"Shifting", icon: < CiDeliveryTruck />, color: 'crimson'},
+        { name:"Plumbing", icon: < MdOutlinePlumbing />, color: 'orange'},
+        { name:"Electric", icon: < MdElectricBolt />, color: 'blue'},
+    ]
+
+    return (
+
+        <div className={style.categories}>
+
+            {categories.map((category) => (
+                <button key={category.name} className={style.btn}>
+                    <div className={style.icon} style={{color: category.color}}> {category.icon} </div>
+                    <div className={style.name}> {category.name}</div>
+                </button>
+            ))}
+
+        </div >
+    )
+}
+
+export default Category;
