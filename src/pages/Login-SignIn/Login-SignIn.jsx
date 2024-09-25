efee8ed7282b6ceee9f297abde3bd6aca5722530
Laurynas <klumbyslaurynas@gmail.com>
2024-09-25 17:24:16 +0300

    login-signIn

--- src/pages/Login-SignIn/Login-SignIn.jsx
+++ src/pages/Login-SignIn/Login-SignIn.jsx
@@ -1,7 +1,19 @@
+import { useNavigate } from "react-router-dom"
+
 export function Login(){
+    const navigate = useNavigate();
+
+    function handleLogin(){
+        navigate("/")
+    }
+    function handleSignIn(){
+        navigate("#")
+    }
     return (
         <div>
             <h1>Login page</h1>
+            <button onClick={handleLogin}>login</button>
+            <p>If you dont have account signIn <button onClick={handleSignIn}>Sign In</button></p>
         </div>
     )
 }
