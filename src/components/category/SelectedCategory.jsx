160ea7472a4093de3d98a04be2ded987bf2f7da3
Laurynas <klumbyslaurynas@gmail.com>
2024-09-28 20:59:26 +0300

    update

--- src/components/category/SelectedCategory.jsx
+++ src/components/category/SelectedCategory.jsx
@@ -3,12 +3,15 @@ import Category2 from "./Category2";
 import { CategoriesCards } from "./CategoryCard";
 import style from "./SelectedCategory.module.scss";
 import categoryCardStyle from "./CategoryCard.module.scss";
+import { FaHeart } from "react-icons/fa";
 
 export function SelectedCategory() {
     const { category } = useParams();
 
     const filteredCards = CategoriesCards.filter(categoryItem => categoryItem.category === category);
 
+
+    
     return (
         <div className={style.selectedCategory}>
             <aside className={style.left}>
@@ -19,6 +22,9 @@ export function SelectedCategory() {
                     {filteredCards.map((categoryItem, index) => (
                         <div key={index} className={categoryCardStyle.card}>
                             <img src={categoryItem.img} alt={categoryItem.category} />
+
+                            <button className={categoryCardStyle.likeBtn}><FaHeart /></button>
+
                             <div className={style.info}>
                                 <h3 className={categoryCardStyle.category}>{categoryItem.category}</h3>
                                 <h3 className={categoryCardStyle.company}>{categoryItem.company}</h3>
